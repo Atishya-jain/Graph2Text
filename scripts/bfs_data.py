1bb5df902af7601934ed3c60a56dc8bf871b70ae
Atishya-Jain <atishya.jain@rocketmail.com>
2020-03-02 22:34:37 +0530

    bfs_data modified

--- scripts/bfs_data.py
+++ scripts/bfs_data.py
@@ -2,7 +2,7 @@ import wikipedia
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
-f = open("fb15k/entity_mid_name_type_typeid.txt", "r")
+f = open("../fb15k/entity_mid_name_type_typeid.txt", "r")
 to_run_on = []
 for i in f.readlines():
 	a,b,c,d = i.strip("\n").split("\t")
@@ -10,16 +10,17 @@ for i in f.readlines():
 		to_run_on.append([(a,0)])
 f.close()
 
-# to_run_on = [[("/m/025n3p",0)]]
+to_run_on = [[("/m/025n3p",0)]]
 def wiki_extract(title):	
 	title = " ".join(title.split('_'))
 	ny = wikipedia.page(title)
-	text = (' '.join(ny.summary.split('\n')))[:500]
+	text = (' '.join(ny.summary.split('\n')))
 	tokenized_text = word_tokenize(text)
-	return ' '.join(tokenized_text).lower()
+	full_text = ' '.join(tokenized_text).lower()
+	return full_text.split('.')
 
 def graph(q):
-	f = open("fb15k/mid2wikipedia_cleaned.tsv", "r")
+	f = open("../fb15k/mid2wikipedia_cleaned.tsv", "r")
 	mid2wiki = {}
 	for i in f.readlines():
 		a,b,c = i.strip("\n").split("\t")
@@ -27,7 +28,7 @@ def graph(q):
 	edges = {}
 	f.close()
 	my_name = mid2wiki[q[0][0]][1]
-	for relations in ["fb15k/train.txt"]:
+	for relations in ["../fb15k/train.txt"]:
 		f = open(relations, "r")
 		for i in f.readlines():
 			e1,r,e2 = i.strip("\n").split("\t")
@@ -56,14 +57,37 @@ def graph(q):
 iteration = 0
 count = 0
 total = 0
+data = []
 for q in to_run_on:
-	if iteration%10 == 1:
-		print("-----------------------------------------", iteration)
-		print("Match percent:", (count/total)*100)
+	# if iteration%10 == 1:
+		# print("-----------------------------------------", iteration)
+		# print("Match percent:", (count/total)*100)
 	iteration += 1
 	graph_ents, my_name = graph(q)
-	wiki_ents = wiki_extract(my_name)
-	total += len(wiki_ents.split())/2
-	for i in graph_ents:
-		if i in wiki_ents:
-			count += 1
+	sentences = wiki_extract(my_name)
+	# print(graph_ents)
+	final_sentence = []
+	selected_ents = []
+	total_characters = 0
+	# print(sentences)
+	for i in sentences:
+		flag = False
+		total_characters += len(i)
+		for j in graph_ents:
+			if j in i:
+				if not flag:
+					final_sentence.append(i.strip())
+					flag = True
+				selected_ents.append(j)
+				# break
+		# if total_characters >= 500:
+		# 	break
+	final_sentence = ' '.join(final_sentence)
+	# data.append((my_name, set(selected_ents), final_sentence))
+	print(set(selected_ents))
+	print(final_sentence)
+	# print((my_name, set(selected_ents), final_sentence))
+	# total += len(final_sentence.split())/2
+	# for i in graph_ents:
+	# 	if i in final_sentence:
+	# 		count += 1
