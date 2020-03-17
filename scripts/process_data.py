ef1f02f661c6f56b9eb6c4823386aea865b99510
Atishya Jain <atishya.jain@rocketmail.com>
2020-03-17 20:14:06 +0530

    Added filtering data script

---
+++ scripts/process_data.py
@@ -0,0 +1,19 @@
+with open('../GraphWriter/data/cur_data.train.tsv') as f:
+	f2 = open('train.tsv', 'w')
+	l = f.readlines()
+	maxi = 0
+	count = 0
+	for line in l:
+		parts = line.split('\t')
+		length = len(parts[3].split(';'))
+		if length > 300:
+			count += 1
+		else:
+			print(line, file=f2, end='')
+
+		if length >= maxi:
+			maxi = length
+
+	print(count)
+	print(maxi)
+		# break
